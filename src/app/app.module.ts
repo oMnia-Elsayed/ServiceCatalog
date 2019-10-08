0c6a6803d4d869fccf5548dd5ae51a4f44822aad
oMnia Elsayed <Omnia.Morsi@naseej.com>
2019-10-08 15:54:15 +0200

    createApp - dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Shared/header/header.component';
+import { SearchCriteriaComponent } from './Features/search-criteria/search-criteria.component';
+import { ListComponent } from './Features/Service/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchCriteriaComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
